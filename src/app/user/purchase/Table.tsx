fa8dfe90632df6e33b32dbf3895e57472484ea6d
D4rzDev <darelhonrejas86@gmail.com>
2024-09-30 17:30:41 +0800

    push

--- src/app/user/purchase/Table.tsx
+++ src/app/user/purchase/Table.tsx
@@ -119,7 +119,7 @@ export default function PurchaseHistoryTable() {
               <TableRow key={index}>
                 <TableCell className="text-center">{new Date(item.createdAt).toDateString()}</TableCell>
                 <TableCell className=' text-center'>₱ {item.amount.toLocaleString()}</TableCell>
-                <TableCell className=' text-center'>{item.minertype}</TableCell>
+                <TableCell className=' text-center'>{item.minertype === 'quick_miner' && 'Quick Miner' || item.minertype === 'rapid_lane' && 'Rapid Miner' || item.minertype === 'swift_lane' && 'Swift Miner'}</TableCell>
               </TableRow>
             ))}
             </>
