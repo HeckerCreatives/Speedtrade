50f6d40091b2e588bae73444daf78ac85f45a53c
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2024-11-05 13:32:01 +0800

    Update Faq.tsx

--- src/app/user/faq/Faq.tsx
+++ src/app/user/faq/Faq.tsx
@@ -14,12 +14,12 @@ export default function Faq() {
         <div className=' w-full max-w-[1040px] flex flex-col gap-2 items-center justify-center p-6'>
 
             <img src="/full.png" alt="" className=' w-[250px] md:w-[350px]' />
-            <p className=' text-lg text-slate-300 text-center mb-10'>Speedtrade is a trading system that allows players to purchase a rig miner, which helps them trade assets quickly. It uses smart algorithms to maximize profits in a competitive market.</p>
+            <p className=' text-lg text-slate-300 text-center mb-10'>Speedmine is a trading system that allows players to purchase a rig miner, which helps them trade assets quickly. It uses smart algorithms to maximize profits in a competitive market.</p>
             <Accordion type="single" collapsible className=' w-full'>
             <AccordionItem value="item-1">
-                <AccordionTrigger className=' w-full'>What is speedtrade?</AccordionTrigger>
+                <AccordionTrigger className=' w-full'>What is Speedmine?</AccordionTrigger>
                 <AccordionContent>
-                    Speedtrade is a trading system where players buy a rig miner, enabling them to trade assets quickly and efficiently by using advanced algorithms to optimize profits in a competitive market
+                    Speedmine is a trading system where players buy a rig miner, enabling them to trade assets quickly and efficiently by using advanced algorithms to optimize profits in a competitive market
                 </AccordionContent>
             </AccordionItem>
             </Accordion>
@@ -28,7 +28,7 @@ export default function Faq() {
 
             <Accordion type="single" collapsible className=' w-full'>
             <AccordionItem value="item-1">
-                <AccordionTrigger className=' w-full'>How can i earn money in Speedtrade?</AccordionTrigger>
+                <AccordionTrigger className=' w-full'>How can i earn money in Speedmine?</AccordionTrigger>
                 <AccordionContent>
                     You can earn by purchasing one of the available offers. After the package duration ends, you can claim the corresponding profits based on the investment period and the profit percentage offered by each package. 
                 </AccordionContent>
@@ -52,7 +52,7 @@ export default function Faq() {
             <AccordionItem value="item-1">
                 <AccordionTrigger className=' w-full'>How do i create an account?</AccordionTrigger>
                 <AccordionContent>
-                   To create an account on Speedtrade, you must first join our official communities on platforms like Discord, Facebook Groups, or other social media channels indicated in the website. Once you are part of the community, reach out to any of the community leaders or admins. These individuals are designated to assist new players in getting started, and you can ask them for a referral link. After receiving the referral link, click on it to be redirected to the Speedtrade registration page. There, you will need to fill out the necessary information, including your name, email address, and other required details. Once you have completed the form, submit it to create your account and start your journey in Speedtrade.
+                   To create an account on Speedmine, you must first join our official communities on platforms like Discord, Facebook Groups, or other social media channels indicated in the website. Once you are part of the community, reach out to any of the community leaders or admins. These individuals are designated to assist new players in getting started, and you can ask them for a referral link. After receiving the referral link, click on it to be redirected to the Speedmine registration page. There, you will need to fill out the necessary information, including your name, email address, and other required details. Once you have completed the form, submit it to create your account and start your journey in Speedmine.
                 </AccordionContent>
             </AccordionItem>
             </Accordion>
@@ -62,9 +62,9 @@ export default function Faq() {
 
             <Accordion type="single" collapsible className=' w-full'>
             <AccordionItem value="item-1">
-                <AccordionTrigger className=' w-full'>What types of rig miner can i buy in Speedtrade?</AccordionTrigger>
+                <AccordionTrigger className=' w-full'>What types of rig miner can i buy in Speedmine?</AccordionTrigger>
                 <AccordionContent>
-                    Speedtrade offers a wide variety of rig miners, categorized into 3 types Quick lane, Swift Lane and Rapid lane. Each type offers different earnings based on the rig miner you purchase. The Quick Lane provides a 30% profit over a 5 days earning period, with a minimum investment of ₱500 and a maximum of ₱2,000. The Swift Lane offers an 80% profit over 10 days, with investment amounts ranging from ₱2,000 to ₱20,000. Lastly, the Rapid Lane delivers a 200% profit over a 20 days period, with a minimum investment of ₱20,000 and a maximum of ₱2,000,000.
+                    Speedmine offers a wide variety of rig miners, categorized into 3 types Quick lane, Swift Lane and Rapid lane. Each type offers different earnings based on the rig miner you purchase. The Quick Lane provides a 20% profit over a 5 days earning period, with a minimum investment of ₱500 and a maximum of ₱2,000. The Swift Lane offers an 60% profit over 10 days, with investment amounts ranging from ₱2,000 to ₱20,000. Lastly, the Rapid Lane delivers a 150% profit over a 20 days period, with a minimum investment of ₱20,000 and a maximum of ₱2,000,000.
                 </AccordionContent>
             </AccordionItem>
             </Accordion>
@@ -73,7 +73,7 @@ export default function Faq() {
             <AccordionItem value="item-1">
                 <AccordionTrigger className=' w-full'>Can i have multiple accounts? </AccordionTrigger>
                 <AccordionContent>
-                    Players are allowed to have multiple accounts in Speedtrade, but each account must be registered with a unique Gmail address. You can sign up and manage multiple accounts, provided that each is linked to a different Gmail address. This allows you to explore different strategies or keep your progress separate. Just make sure to follow the system rules for each account to prevent any issues
+                    Players are allowed to have multiple accounts in Speedmine, but each account must be registered with a unique Gmail address. You can sign up and manage multiple accounts, provided that each is linked to a different Gmail address. This allows you to explore different strategies or keep your progress separate. Just make sure to follow the system rules for each account to prevent any issues
                 </AccordionContent>
             </AccordionItem>
             </Accordion>
@@ -90,9 +90,9 @@ export default function Faq() {
 
             <Accordion type="single" collapsible className=' w-full'>
             <AccordionItem value="item-1">
-                <AccordionTrigger className=' w-full'>How can I invite friends to play Speedtrade?</AccordionTrigger>
+                <AccordionTrigger className=' w-full'>How can I invite friends to play Speedmine?</AccordionTrigger>
                 <AccordionContent>
-                   You can invite friends to join Speedtrade by sharing your unique referral link found in the system's invitation section. To locate your referral link, navigate to your dashboard and click on the referral link next to your username. After you have your link, share it with friends via social media, email, or messaging apps. When your friends use the referral link to sign up and purchase one of the rig miners, you'll earn rewards based on their profit percentage.
+                   You can invite friends to join Speedmine by sharing your unique referral link found in the system's invitation section. To locate your referral link, navigate to your dashboard and click on the referral link next to your username. After you have your link, share it with friends via social media, email, or messaging apps. When your friends use the referral link to sign up and purchase one of the rig miners, you'll earn rewards based on their profit percentage.
                 </AccordionContent>
             </AccordionItem>
             </Accordion>
