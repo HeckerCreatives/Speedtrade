42a557122086b2df6386b970cc8d93a506973320
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2025-02-02 00:21:21 +0800

    Update routes.tsx

--- src/app/types/routes.tsx
+++ src/app/types/routes.tsx
@@ -12,12 +12,12 @@ export const user = [
 export const superadmin = [
     {name:'Dashboard', icon: <LayoutGrid width={15} height={15}/>, route:'/superadmin/dashboard'},
     {name:'Manage Account', icon: <Users width={15} height={15}/>, route:'/superadmin/manageaccount'},
-    // {name:'Sales', icon: <ChartColumn width={15} height={15}/>, route:'/superadmin/sales'},
-    // {name:'Maintenance', icon: <Cable width={15} height={15}/>, route:'/superadmin/maintenance'},
+    {name:'Sales', icon: <ChartColumn width={15} height={15}/>, route:'/superadmin/sales'},
+    {name:'Maintenance', icon: <Cable width={15} height={15}/>, route:'/superadmin/maintenance'},
     {name:'Deposit', icon: <HandCoins width={15} height={15}/>, route:'/superadmin/deposit'},
     {name:'Withdrawal', icon: <WalletMinimal width={15} height={15}/>, route:'/superadmin/withdrawal'},
     {name:'Price Pool', icon: <AlignEndHorizontal width={15} height={15}/>, route:'/superadmin/prizepool'},
-    // {name:'Miner', icon: <Pickaxe width={15} height={15}/>, route:'/superadmin/miner'},
+    {name:'Miner', icon: <Pickaxe width={15} height={15}/>, route:'/superadmin/miner'},
     {name:'Master Key', icon: <TicketCheck width={15} height={15}/>, route:'/superadmin/masterkey'},
     {name:'Settings', icon: <Cog width={15} height={15}/>, route:'/superadmin/settings'},
 ]
